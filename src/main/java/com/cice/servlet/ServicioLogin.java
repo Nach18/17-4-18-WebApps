1f23e2052952247e4545cf5a9be37203948f8f3b
Nach18 <ignaciolopezpuerto@gmail.com>
2018-04-19 21:15:51 +0200

    ERRORES HABITUALES: 1) El proyecto no tiene la dependecia de maven 2) El Statment debe llamar a user y pass ya creados antes en el proyecto como '"+user+"', '"+pass+"' 3) La columna de ID debe ser unica por lo que ha de ser primaria y AUTO_INCREMENTABLE

--- src/main/java/com/cice/servlet/ServicioLogin.java
+++ src/main/java/com/cice/servlet/ServicioLogin.java
@@ -5,6 +5,8 @@ package com.cice.servlet;
 2) El Statment debe llamar a user y pass ya creados antes en el proyecto como '"+user+"', '"+pass+"'
 3) La columna de ID debe ser unica por lo que ha de ser primaria y AUTO_INCREMENTABLE*/
 
+
+
 import com.sun.net.httpserver.HttpsServer;
 
 import javax.servlet.ServletException;
@@ -12,10 +14,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.io.PrintWriter;
+import java.sql.*;
 
 public class ServicioLogin extends HttpServlet{
 
@@ -26,6 +26,8 @@ public class ServicioLogin extends HttpServlet{
         String user = req.getParameter("user");
         String pass = req.getParameter("pass");
 
+        PrintWriter writer = resp.getWriter();
+
         try {
             Class.forName("com.mysql.jdbc.Driver");
 
@@ -33,10 +35,22 @@ public class ServicioLogin extends HttpServlet{
 
             Statement statment = connection.createStatement();
 
-            String sql = "INSERT INTO prueba VALUES (null, '"+user+"', '"+pass+"')";
+            //String sql = "INSERT INTO prueba VALUES (null, '"+user+"', '"+pass+"')";
 
-            statment.executeUpdate(sql);
+            String sql = "SELECT * FROM prueba WHERE user = '"+user+"' AND pass = '"+pass+"'";
 
+            //statment.executeUpdate(sql);
+            ResultSet rs= statment.executeQuery(sql);
+            if (rs.first()) {
+                //coinciden!
+                writer.print("Bienvenido: " + user);
+            } else {
+                //no coinciden!
+                writer.print("403 - DENIED ACCESS, GET OUT OF HERE MOTHERFU****!");
+            }
+
+            rs.close();
+            writer.close();
             connection.close();
             statment.close();
 
