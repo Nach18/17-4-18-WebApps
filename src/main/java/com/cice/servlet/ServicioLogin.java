8fb33114ece45d917ede0d15168b4e1c76952832
Nach18 <ignaciolopezpuerto@gmail.com>
2018-04-19 20:15:54 +0200

    ERRORES HABITUALES: 1) El proyecto no tiene la dependecia de maven 2) El Statment debe llamar a user y pass ya creados antes en el proyecto como '"+user+"', '"+pass+"' 3) La columna de ID debe ser unica por lo que ha de ser primaria y AUTO_INCREMENTABLE

--- src/main/java/com/cice/servlet/ServicioLogin.java
+++ src/main/java/com/cice/servlet/ServicioLogin.java
@@ -1,5 +1,10 @@
 package com.cice.servlet;
 
+/*ERRORES HABITUALES:
+1) El proyecto no tiene la dependecia de maven
+2) El Statment debe llamar a user y pass ya creados antes en el proyecto como '"+user+"', '"+pass+"'
+3) La columna de ID debe ser unica por lo que ha de ser primaria y AUTO_INCREMENTABLE*/
+
 import com.sun.net.httpserver.HttpsServer;
 
 import javax.servlet.ServletException;
@@ -7,6 +12,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ServicioLogin extends HttpServlet{
 
@@ -16,6 +25,27 @@ public class ServicioLogin extends HttpServlet{
         System.out.println("llamando al Post");
         String user = req.getParameter("user");
         String pass = req.getParameter("pass");
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:8889/Prueba", "root", "root");
+
+            Statement statment = connection.createStatement();
+
+            String sql = "INSERT INTO prueba VALUES (null, '"+user+"', '"+pass+"')";
+
+            statment.executeUpdate(sql);
+
+            connection.close();
+            statment.close();
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
 
     }
 }
